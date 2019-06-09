314a25e5d973c4587a7768d605883414363b259c
Adi Ronen <adironenn@gmail.com>
2019-06-09 15:14:06 +0300

    Set ruby version in ps1

--- bashrc/sort/ps1.sh
+++ bashrc/sort/ps1.sh
@@ -8,4 +8,4 @@ which git-prompt > /dev/null 2> /dev/null && PS1GITPROMPT='[$(git-prompt)] '
 which node       > /dev/null 2> /dev/null && PS1NODEVERSION='[$(node -v | sed "s/^v//")] '
 which ruby       > /dev/null 2> /dev/null && PS1RUBYVERSION='[$(ruby -v | sed "s/^ruby //" | sed "s/ .*//")] '
 
-PS1="\t $PS1NODEVERSION$PS1GITPROMPT\w> "
+PS1="\t $PS1RUBYVERSION$PS1GITPROMPT\w> "
